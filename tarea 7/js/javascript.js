1ba336fb46bdc9995ba5133d9262b18154f3f2db
wargenalu <ingargenal@gmail.com>
2023-06-02 19:27:46 -0600

    Actualizacion de archivo js(botones inicio y formulario funcionan)

--- tarea 7/js/javascript.js
+++ tarea 7/js/javascript.js
@@ -1,19 +1,33 @@
 
-let botonSaludar = document.getElementById("saludar")
-let botonCerrar = document.getElementById("boton-cerrar")
-let mensajeBienvenida =document.getElementById("mensaje-bienvenida")
-
+var mensajeBienvenida = document.getElementById("mensaje-bienvenida")
+var seccionBienvenida = document.getElementById("bienvenida")
+var seccionFormulario = document.getElementById("formulario")
+var botonFormulario = document.getElementById("boton-formulario")
+var botonSaludar = document.getElementById("saludar")
+var botonCerrar = document.getElementById("boton-cerrar")
+var botonInicio = document.getElementById("boton-inicio")
 
 botonSaludar.addEventListener("click", mostrarBienvenida)
 botonCerrar.addEventListener("click", cerrarBienvenida)
-
+botonInicio.addEventListener("click", inicio)
+botonFormulario.addEventListener("click", formulario)
 
 function mostrarBienvenida(){
-    mensajeBienvenida.style.display = "block"
+  mensajeBienvenida.style.display = "block"
 }
 
 function cerrarBienvenida(){
-  mensajeBienvenida.style.display = "none"
-    
 
+  mensajeBienvenida.style.display = "none"
+}
+
+function inicio(){
+
+  seccionBienvenida.style.display="block"
+  seccionFormulario.style.display="none"
+}
+
+function formulario(){
+  seccionBienvenida.style.display="none"
+  seccionFormulario.style.display="block"
 }
